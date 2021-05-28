db89875843e66af4daceba94da99310e4c29096f
Lizzette Salmeron <lsalmer000@citymail.cuny.edu>
2021-05-28 17:25:34 -0400

    Changed card values to string, added def to view card format, Testing

--- Card.py
+++ Card.py
@@ -8,8 +8,9 @@ class Card:
             self.AJQK_value = 11
         elif value in ['J', 'Q', 'K']:
             self.AJQK_value = 10
-        elif (value in [2, 3, 4, 5, 6, 7, 8, 9, 10]):
-            self.AJQK_value = value
+        elif (value in ['2', '3', '4', '5', '6', '7', '8', '9', '10']):
+            # value in [2, 3, 4, 5, 6, 7, 8, 9, 10]
+            self.AJQK_value = int(value)
 
     def get_suit(self):
         return self.suit
@@ -19,4 +20,15 @@ class Card:
 
     def get_AJQK_value(self):
         return self.AJQK_value
-    
+
+    #Add a def to view the format of the card
+    def __str__(self):
+        return "{}_{}".format(self.value, self.suit)
+
+
+# Testing 
+# card1 = Card('S', 4)
+# # # print(card1.get_suit())
+# print(card1)
+
+
