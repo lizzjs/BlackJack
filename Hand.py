cac3bb35c4564e3c11592915c365805e078518a6
Lizzette Salmeron <lsalmer000@citymail.cuny.edu>
2021-06-01 11:27:45 -0400

    Added method for calculating hand, taking Ace values into account

--- Hand.py
+++ Hand.py
@@ -7,22 +7,47 @@ class Hand:
     #Checking which cards are in hand
     def __str__(self):
         if self.cards:
-            rep = ""
+            rep_hand = ""
             for card in self.cards:
-                rep += str(card) + " "
+                rep_hand += str(card) + " "
         else:
-            rep = "<empty>"
-        return rep
+            rep_hand = "<empty>"
+        return rep_hand
     
     def add(self, card):
         self.cards.append(card)
 
-# Testing: Add two cards to hand, check the value of J card
-# card1 = Card('S', 4)
-# card2 = Card('D', 'J')
+    def calculate_hand(self):
+        self.value = 0
+        ace_bool = False
+
+        for card in self.cards:
+            if card.value.isnumeric():
+                self.value += int(card.value)
+            else:
+                if card.value == 'A':
+                    ace_bool = True
+                    self.value += 11
+                else:
+                    self.value += 10
+
+        if ace_bool and self.value>21:
+            self.value -= 10 #make Ace = 1 instead
+
+        
+    def get_value(self):
+        self.calculate_hand()
+        return self.value
+
+
+# Testing: Add two cards to hand, check the value of A card
+# card1 = Card('S', '4')
+# card2 = Card('D', 'A')
 
 # hand1 = Hand()
 # hand1.add(card1)
 # hand1.add(card2)
 # print(hand1)
-# print(card2.get_AJQK_value())
+# # print(card2.get_Cardvalue())
+# # print(card2.get_AJQK_value()) #might be redundant, comment out values in Card class
+# print(hand1.get_value())
